6973066162008d30cf1da248ef82d989d673e150
Joshua Lee <joshua.l@mckinneysteamacademy.org>
2024-07-24 19:53:15 -0500

    I don't know what I'm doing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTest.java
@@ -1,30 +1,43 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.Trajectory;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
+//This autonomous is for testing Road Runner
 @Autonomous (name="Testing Road Runner", group="")
 public class AutonTest extends LinearOpMode {
+    MecanumDrive drive;
+    private void initializeMotors() {
+        drive.leftFront.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        drive.leftBack.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        drive.rightFront.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+        drive.rightBack.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+    }
+    double StartPoseX = -5.0;
+    double StartPoseY = -5.0;
+    double StartPoseT = -45.0;
+    @Override
     public void runOpMode() {
-    SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Pose2d StartPose = new Pose2d(StartPoseX, StartPoseY, Math.toRadians(StartPoseT));
+        //We are following this: https://rr.brott.dev/docs/v1-0/guides/centerstage-auto/ while working on this. Setting starting pose
+        MecanumDrive drive = new MecanumDrive(hardwareMap, StartPose);
+        //initializeMotors();
 
-     // We want to start the bot at x: 10, y: -8, heading: 90 degrees
-    Pose2d startPose = new Pose2d(10, -8, Math.toRadians(90));
+        Action trajectoryAction1;
 
-      drive.setPoseEstimate(startPose);
-      Trajectory traj1 = drive.trajectoryBuilder(startPose)
-        .splineTo(new Vector2d(20, 9), Math.toRadians(45))
-        .build();
-
-      Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
-         .splineTo(new Vector2d(20, 9), Math.toRadians(45))
-        .build();
-
-  drive.followTrajectory(traj1);
-  drive.followTrajectory(traj2);
-}
+        trajectoryAction1 = drive.actionBuilder(drive.pose)
+//                .strafeTo(new Vector2d(10, -5))
+                .splineTo(new Vector2d(30, 30), Math.toRadians(72))
+                .build();
+        waitForStart();
+        //while (opModeIsActive()) {
+            Actions.runBlocking(trajectoryAction1);
+        //}
     }
 }
