05036236d82c49bae386b30b50d617fd629e307c
Mercs <test@abc.com>
2024-10-05 18:19:03 -0500

    Rigvedh -- Driver Assist: -Created State Machine -Rotation to Wall -Drive Toward Wall -Opens Claw -Lifts Elevator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DEEP_TeleOp_Main_20166.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DEEP_TeleOp_Main_20166.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -39,6 +40,9 @@ public class DEEP_TeleOp_Main_20166 extends LinearOpMode {
     private static final int ELEVATOR_TEST_CHANGE = 50;
     private static final double GRABBER_OPEN_POSITION = 0.7;
     private static final double GRABBER_CLOSE_POSITION = 0.95;
+    private static final int OBSERVATION_PICKUP_LEFT_DISTANCE = 500;
+    private static final int OBSERVATION_PICKUP_FRONT_DISTANCE = 45;
+
     private int elevatorMoveTo = 0;
     private double translateX;
     private double translateY;
@@ -66,7 +70,10 @@ public class DEEP_TeleOp_Main_20166 extends LinearOpMode {
 
     NormalizedColorSensor colorSensor;
     RevBlinkinLedDriver blinkinLedDriver;
+    Rev2mDistanceSensor leftDistanceSensor;
+    Rev2mDistanceSensor frontDistanceSensor;
 
+    private boolean driverAssistPickup = false;
 
     public void initializeDriveMotors()
     {
@@ -100,7 +107,12 @@ public class DEEP_TeleOp_Main_20166 extends LinearOpMode {
         imu.resetYaw();
 
         //Initialize the color sensor
-        colorSensor = hardwareMap.get(NormalizedColorSensor.class, "sensor_color");
+        colorSensor = hardwareMap.get(NormalizedColorSensor.class, "colorSensor");
+
+        //Initialize distance sensors
+        leftDistanceSensor = hardwareMap.get(Rev2mDistanceSensor.class, "leftDistanceSensor");
+        frontDistanceSensor = hardwareMap.get(Rev2mDistanceSensor.class, "frontDistanceSensor");
+
     }
 
     private void initializeSubSystems() throws InterruptedException {
@@ -155,6 +167,11 @@ public class DEEP_TeleOp_Main_20166 extends LinearOpMode {
         return orientation.getYaw(AngleUnit.RADIANS);
     }
 
+    public double getHeadingDegrees() {
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        return orientation.getYaw(AngleUnit.DEGREES);
+    }
+
     private static double[] rotatePoint(double xPoint, double yPoint, double angle) {
         double[]  Result = new double[2];
 
@@ -203,6 +220,11 @@ public class DEEP_TeleOp_Main_20166 extends LinearOpMode {
         grabberOpen = gamepad2.x;
         grabberClose = gamepad2.b;
 
+        if (gamepad1.x)
+            driverAssistPickup = true;
+        else
+            driverAssistPickup = false;
+
         //buttons
 
         //if(gamepad1.left_bumper) arm.setPosition(-786); else arm.setPosition(0);
@@ -210,11 +232,11 @@ public class DEEP_TeleOp_Main_20166 extends LinearOpMode {
 
     private void updateDashboard()
     {
-//        telemetry.addData("FL",frontLeftDrive.getVelocity());
-//        telemetry.addData("FR",frontRightDrive.getVelocity());
-//        telemetry.addData("BL",backLeftDrive.getVelocity());
-//        telemetry.addData("BR",backRightDrive.getVelocity());
-//        telemetry.addData("Heading", getHeading());
+//        telemetry.addData("FL",frontLeftDrive.getCurrentPosition());
+//        telemetry.addData("FR",frontRightDrive.getCurrentPosition());
+//        telemetry.addData("BL",backLeftDrive.getCurrentPosition());
+//        telemetry.addData("BR",backRightDrive.getCurrentPosition());
+        telemetry.addData("Heading", getHeadingDegrees());
 
         telemetry.addLine("\n");
         telemetry.addData("Elevator Pos", robotElevator.getPosition());
@@ -222,6 +244,9 @@ public class DEEP_TeleOp_Main_20166 extends LinearOpMode {
 
         telemetry.addLine("\n");
         telemetry.addData("Sample detected", getSampleColor());
+
+        telemetry.addData("Left distance", leftDistanceSensor.getDistance(DistanceUnit.MM));
+        telemetry.addData("Front distance", frontDistanceSensor.getDistance(DistanceUnit.MM));
         updateTelemetry(telemetry);
     }
     private void updateDrivebaseMotors(double FLMP, double FRMP, double BLMP, double BRMP)
@@ -268,48 +293,145 @@ public class DEEP_TeleOp_Main_20166 extends LinearOpMode {
         BLMP = BLPFB + BLPLR + BLPR;
         BRMP = BRPFB + BRPLR + BRPR;
     }
+    private void setElevator(int position)
+    {
+        robotElevator.setPosition(position);
+        elevatorMoveTo = position;
+    }
     public void updateElevator()
     {
         if (elevatorMoveBottom == true)
         {
-            robotElevator.setPosition(ELEVATOR_BOTTOM_POSITION);
-            elevatorMoveTo = ELEVATOR_BOTTOM_POSITION;
+            setElevator(ELEVATOR_BOTTOM_POSITION);
         }
         else if (elevatorMoveTop == true)
         {
-            robotElevator.setPosition(ELEVATOR_TOP_RUNG_PLACE);
-            elevatorMoveTo = ELEVATOR_TOP_RUNG_PLACE;
+            setElevator(ELEVATOR_TOP_RUNG_PLACE);
         }
         else if (elevatorMoveLow == true)
         {
-            robotElevator.setPosition(ELEVATOR_SPECIMEN_PICKUP);
-            elevatorMoveTo = ELEVATOR_SPECIMEN_PICKUP;
+            setElevator(ELEVATOR_SPECIMEN_PICKUP);
         }
         else if (elevatorMoveHigh == true)
         {
-            robotElevator.setPosition(ELEVATOR_TOP_RUNG_RELEASE);
-            elevatorMoveTo = ELEVATOR_TOP_RUNG_RELEASE;
+            setElevator(ELEVATOR_TOP_RUNG_RELEASE);
         }
         else if (gamepad2.right_bumper == true)
         {
-            robotElevator.setPosition(robotElevator.getPosition() + ELEVATOR_TEST_CHANGE);
+            setElevator(robotElevator.getPosition() + ELEVATOR_TEST_CHANGE);
         }
         else if (gamepad2.left_bumper == true)
         {
-            robotElevator.setPosition(robotElevator.getPosition() - ELEVATOR_TEST_CHANGE);
+            setElevator(robotElevator.getPosition() - ELEVATOR_TEST_CHANGE);
         }
+    }
+    private void setGrabberServo (boolean state)
+    {
+        if (state)
+            robotGrabber.setPosition(GRABBER_OPEN_POSITION);
+        else
+            robotGrabber.setPosition(GRABBER_CLOSE_POSITION);
     }
     public void updateGrabber()
     {
        if (grabberOpen == true)
        {
-           robotGrabber.setPosition(GRABBER_OPEN_POSITION);
+           setGrabberServo(true);
        }
        else if (grabberClose == true)
         {
-            robotGrabber.setPosition(GRABBER_CLOSE_POSITION);
+            setGrabberServo(false);
         }
     }
+
+    private boolean rotateRobotToHeading(double heading)
+    {
+        double rotateSpeed = 0.45;
+        double tolerance = 3;
+        double headingError = getHeadingDegrees() - heading;
+        if (headingError < -180)
+            headingError = headingError + 360;
+        else if (headingError > 180)
+            headingError = headingError - 360;
+        if (headingError < -tolerance)
+        {
+            updateDrivebaseMotors(rotateSpeed, -rotateSpeed, rotateSpeed,-rotateSpeed);
+            return false;
+        }
+        else if (headingError > tolerance)
+        {
+            updateDrivebaseMotors(-rotateSpeed, rotateSpeed, -rotateSpeed, rotateSpeed);
+            return false;
+        }
+        else
+        {
+            updateDrivebaseMotors(0, 0, 0, 0);
+            return true;
+        }
+    }
+    private boolean driveRobotToDistanceFrom (double distance)
+    {
+        double currentDistance = frontDistanceSensor.getDistance(DistanceUnit.MM);
+        double driveSpeed = -0.25;
+        if (currentDistance > distance)
+        {
+            updateDrivebaseMotors(driveSpeed, driveSpeed, driveSpeed, driveSpeed);
+            return false;
+
+        }
+            else
+        {
+            updateDrivebaseMotors(0, 0, 0, 0);
+            return true;
+        }
+    }
+    private static final int STATE_WAIT = 0;
+    private static final int STATE_FACE_WALL = 1;
+    private static final int STATE_DRIVE_TO_WALL_1 = 2;
+    private static final int STATE_WAIT_RELEASE = 3;
+    private static final int STATE_STRAFE_TO_WALL = 4;
+
+    private int currentDriverAssistState = STATE_WAIT;
+
+    private void processStateMachine()
+    {
+        boolean done = false;
+        if (currentDriverAssistState == STATE_WAIT)
+        {
+            if (driverAssistPickup)
+            {
+                currentDriverAssistState = STATE_FACE_WALL;
+            }
+        }
+        else if (currentDriverAssistState == STATE_FACE_WALL)
+        {
+            done = rotateRobotToHeading(180.0);
+            if (done)
+                currentDriverAssistState = STATE_DRIVE_TO_WALL_1;
+        }
+        else if (currentDriverAssistState == STATE_DRIVE_TO_WALL_1)
+        {
+            done = driveRobotToDistanceFrom(120.0);
+            if (done)
+            {
+                currentDriverAssistState = STATE_STRAFE_TO_WALL;
+                setGrabberServo(true);
+                setElevator(ELEVATOR_SPECIMEN_PICKUP);
+            }
+        }
+        else if (currentDriverAssistState == STATE_WAIT_RELEASE)
+        {
+            if (!driverAssistPickup)
+                currentDriverAssistState = STATE_WAIT;
+        }
+        else if (currentDriverAssistState == STATE_STRAFE_TO_WALL)
+        {
+            done = true;
+            if (done)
+                currentDriverAssistState = STATE_WAIT_RELEASE;
+        }
+    }
+
         public void runOpMode() throws InterruptedException {
         initalizeEverything();
 
@@ -319,9 +441,13 @@ public class DEEP_TeleOp_Main_20166 extends LinearOpMode {
         {
             updateJoysticks();
             calculateDrivebaseSpeed();
-            updateDrivebaseMotors(FLMP, FRMP, BLMP, BRMP);
-            updateElevator();
-            updateGrabber();
+            if (currentDriverAssistState == STATE_WAIT)
+            {
+                updateDrivebaseMotors(FLMP, FRMP, BLMP, BRMP);
+                updateElevator();
+                updateGrabber();
+            }
+            processStateMachine();
             updateDashboard();
         }
 
