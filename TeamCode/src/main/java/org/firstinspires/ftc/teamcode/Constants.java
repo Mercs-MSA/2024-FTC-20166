8e8b29a9012684f65fb93b900d365b0e3839de00
Joshua Lee <joshua.l@mckinneysteamacademy.org>
2024-10-14 17:10:08 -0500

    Lift Roadrunner Actions are done (with no testing though lol)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -2,4 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 public class Constants {
     public static double slideTickPerIn = 30.05;
+
+    public static double pivotTickPerDegree = 0;
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadRunnerActions/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadRunnerActions/Lift.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.roadRunnerActions;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad2;
+import static org.firstinspires.ftc.teamcode.Constants.pivotTickPerDegree;
 import static org.firstinspires.ftc.teamcode.Constants.slideTickPerIn;
 
 import androidx.annotation.NonNull;
@@ -14,11 +15,17 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class Lift {
     private DcMotor liftMotor = null;
 
+    private DcMotor pivot = null;
     public Lift(HardwareMap hardwareMap) {
         liftMotor = hardwareMap.get(DcMotor.class, "liftMotor");
         liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        pivot = hardwareMap.get(DcMotor.class, "pivot");
+        pivot.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
+
+    //High Basket Score:
     public class LiftHighBasket implements Action {
         private boolean initialized = false;
 
@@ -37,13 +44,63 @@ public class Lift {
         return new LiftHighBasket();
     }
 
+
+    //High Specimen Score:
     public class LiftHighSpec implements Action {
         private boolean initialized = false;
 
         @Override
         public boolean run(@NonNull TelemetryPacket packet) {
-            if (!initialized) {}
+            if (!initialized) {
+                liftMotor.setPower(.5);
+                liftMotor.setTargetPosition((int)(20 * slideTickPerIn));
+                initialized = true;
+            }
             return true;
         }
+    }
+
+    public Action liftHighSpec() {
+        return new LiftHighSpec();
+    }
+
+
+    //Pivot Up:
+    public class PivotUp implements Action {
+        private boolean initialized = false;
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            if (!initialized) {
+                pivot.setPower(.7);
+                pivot.setTargetPosition(0);
+                initialized = true;
+            }
+            return true;
+        }
+    }
+
+    public Action pivotUp() {
+        return new PivotUp();
+    }
+
+
+    //Pivot Down:
+    public class PivotDown implements Action {
+        private boolean initialized = false;
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            if (!initialized) {
+                pivot.setPower(.7);
+                pivot.setTargetPosition((int)(30 * pivotTickPerDegree));
+                initialized = true;
+            }
+            return true;
+        }
+    }
+
+    public Action pivotDown() {
+        return new PivotDown();
     }
 }
