e02e28a5834cb7d1a38aac87f778319842a6332b
Joshua Lee <joshua.l@mckinneysteamacademy.org>
2024-10-14 15:50:43 -0500

    Changed programs and added a lot of stuff related to odom (sparkfun)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTest.java
@@ -10,7 +10,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-//import org.firstinspires.ftc.teamcode.roadRunnerActions.Lift;
+import org.firstinspires.ftc.teamcode.roadRunnerActions.Lift;
+
 
 //This autonomous is for testing Road Runner
 @TeleOp(name="Testing Road Runner", group="")
@@ -23,14 +24,14 @@ public class AutonTest extends LinearOpMode {
         drive.rightBack.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
     }
     double StartPoseX = 0;
-    double StartPoseY = 0;
-    double StartPoseT = 0;
+    double StartPoseY = -60;
+    double StartPoseT = 90;
     @Override
     public void runOpMode() {
         Pose2d StartPose = new Pose2d(StartPoseX, StartPoseY, Math.toRadians(StartPoseT));
         //We are following this: https://rr.brott.dev/docs/v1-0/guides/centerstage-auto/ while working on this. Setting starting pose
         MecanumDrive drive = new MecanumDrive(hardwareMap, StartPose);
-        //Lift lift = new Lift(hardwareMap);
+        Lift lift = new Lift(hardwareMap);
 
         drive.leftFront.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         drive.leftBack.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
@@ -39,37 +40,17 @@ public class AutonTest extends LinearOpMode {
 
         Action trajectoryAction1;
 
-        trajectoryAction1 = drive.actionBuilder(drive.pose)
-//                .strafeTo(new Vector2d(0,48))
-//                .splineTo(new Vector2d(15, 15), Math.toRadians(45))
-//                .splineTo(new Vector2d(37, 37), Math.toRadians(-32))
-//                .splineTo(new Vector2d(35, 15), Math.toRadians(-90))
-//                .splineTo(new Vector2d(0, 0), Math.toRadians(-180))
+        Action trajectoryAction2;
 
-//                .strafeTo(new Vector2d(36,0))
-//                .waitSeconds(1.5)
-//                .strafeTo(new Vector2d(36,36))
-//                .waitSeconds(1.5)
-//                .strafeTo(new Vector2d(60,60))
-//                .waitSeconds(1.5)
-//                .turn(Math.toRadians(-45))
-//                .splineTo(new Vector2d(30, 30), Math.toRadians(-90))
-//                .waitSeconds(3)
-//                .splineTo(new Vector2d(0,0), Math.toRadians(90))
-                .strafeTo(new Vector2d(24,0))
-                .turn(Math.toRadians(90))
+        trajectoryAction1 = drive.actionBuilder(drive.pose)
+                .strafeTo(new Vector2d(0, -32))
+                .strafeTo(new Vector2d(-49, -32))
+                .turn(Math.toRadians(180))
+                .splineTo(new Vector2d(-53, -53), Math.toRadians(215))
                 .build();
         waitForStart();
 
-        Actions.runBlocking(trajectoryAction1);
-        while(opModeIsActive()) {
-            drive.updatePoseEstimate();
-
-            telemetry.addData("X: ", drive.poseOTOS.position.x);
-            telemetry.addData("Y: ", drive.poseOTOS.position.y);
-            telemetry.addData("Theta: ", Math.toDegrees(drive.poseOTOS.heading.toDouble()));
-            telemetry.update();
-        }
-        //Actions.runBlocking(lift.liftExtend());
+//        Actions.runBlocking(trajectoryAction1);
+        Actions.runBlocking(trajectoryAction1, lift.liftExtend());
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadRunnerActions/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadRunnerActions/Lift.java
@@ -1,37 +1,37 @@
-//package org.firstinspires.ftc.teamcode.roadRunnerActions;
-//
-//import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
-//import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad2;
-//
-//import androidx.annotation.NonNull;
-//
-//import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-//import com.acmerobotics.roadrunner.Action;
-//import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.HardwareMap;
-//
-//public class Lift {
-//    private DcMotor liftMotor = null;
-//
-//    public Lift(HardwareMap hardwareMap) {
-//        liftMotor = hardwareMap.get(DcMotor.class, "liftMotor");
-//        liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-//    }
-//
-//    public class LiftExtend implements Action {
-//        private boolean initialized = false;
-//
-//        @Override
-//        public boolean run(@NonNull TelemetryPacket packet) {
-//            if (!initialized) {
-//                liftMotor.setTargetPosition(100);
-//                initialized = true;
-//            }
-//            return true;
-//        }
-//    }
-//
-//    public Action liftExtend() {
-//        return new LiftExtend();
-//    }
-//}
+package org.firstinspires.ftc.teamcode.roadRunnerActions;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad2;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Lift {
+    private DcMotor liftMotor = null;
+
+    public Lift(HardwareMap hardwareMap) {
+        liftMotor = hardwareMap.get(DcMotor.class, "liftMotor");
+        liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public class LiftExtend implements Action {
+        private boolean initialized = false;
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            if (!initialized) {
+                liftMotor.setTargetPosition(100);
+                initialized = true;
+            }
+            return true;
+        }
+    }
+
+    public Action liftExtend() {
+        return new LiftExtend();
+    }
+}
