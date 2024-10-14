3cbbe29bb10dc4f7daedbb857ab57539441b6bce
Joshua Lee <joshua.l@mckinneysteamacademy.org>
2024-10-14 18:15:18 -0500

    constants updated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
     public static double slideTickPerIn = 30.05;
 
     public static double pivotTickPerDegree = 0;
+
+    public static double climberTickPerIn = 1524.39;
 }
